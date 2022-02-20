b2fd0076fc33fe853437ea671f72acafb7dd0fbc
caiconghao <caiconghao@flyfinger.com>
2022-02-20 17:34:36 +0800

    feat: color panel

---
+++ self-files/04/redux-paint/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { beginStroke, endStroke, updateStroke } from "./actions";
+import { drawStroke } from "./canvasUtils";
+import { currentStrokeSelector } from "./selectors";
+
+function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const currentStroke = useSelector(currentStrokeSelector);
+  const isDrawing = !!currentStroke.points.length;
+  const dispatch = useDispatch();
+
+  const getCanvasWithContext = (canvas = canvasRef.current) => {
+    return { canvas, context: canvas?.getContext("2d") };
+  };
+
+  const startDrawing = ({
+    nativeEvent,
+  }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent;
+    dispatch(beginStroke(offsetX, offsetY));
+  };
+
+  const endDrawing = () => {
+    if (isDrawing) {
+      dispatch(endStroke());
+    }
+  };
+
+  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    dispatch(updateStroke(offsetX, offsetY));
+  };
+
+  useEffect(() => {
+    const { context } = getCanvasWithContext();
+    if (!context) {
+      return;
+    }
+    requestAnimationFrame(() =>
+      drawStroke(context, currentStroke.points, currentStroke.color)
+    );
+  }, [currentStroke]);
+
+  return (
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseOut={endDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
+  );
+}
+
+export default App;
---
+++ self-files/04/redux-paint/src/ColorPanel.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setStrokeColor } from "./actions";
+
+const COLORS = ["#000000", "#808080", "#c0c0c0", "#ffffff", "#800000"];
